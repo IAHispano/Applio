4eae15f862e64c61af91eaae6e15468a882b9fa4
kalomaze <66376113+kalomaze@users.noreply.github.com>
2023-07-25 01:31:30 -0500

    Revert "Merge pull request #53 from alexlnkp/main"
    This reverts commit a2ce6d744bb1648bae9ffece82175b9e004a50e1, reversing
    changes made to 91dfaccac8c7b663b1d9d5ef103d578ef058e3d4.

--- extract_f0_print.py
+++ extract_f0_print.py
@@ -19,8 +19,10 @@ f = open("%s/extract_f0_feature.log" % exp_dir, "a+")
 
 DoFormant = False
 
-Quefrency = 0.0
-Timbre = 0.0
+with open("formanting.txt", "r") as fvf:
+    content = fvf.readlines()
+    Quefrency, Timbre = content[1].split("\n")[0], content[2].split("\n")[0]
+
 
 def printt(strr):
     print(strr)
