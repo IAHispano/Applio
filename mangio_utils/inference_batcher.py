86b7cc77d8b919b0f0228c56efb8a6c33cbef5c7
DeiantV <vidaldiscord1@gmail.com>
2023-08-18 19:16:26 -0500

    Deleting All Files

--- mangio_utils/inference_batcher.py
+++
@@ -1,5 +0,0 @@
-# Mangio-RVC-Fork Feature. Splits source audio into multiple segments (nparrays).
-
-from scipy.io import wavfile
-import numpy as np
-import os
--- mangio_utils/segment-outputs/Readme.txt
+++
@@ -1,2 +0,0 @@
-This folder (segment-outputs) is where all segment outputs created from the inference-batcher.py
-are stored temporarily.
