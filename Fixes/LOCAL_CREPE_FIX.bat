b452087e164e7a741045b5511dabbdc23597fec5
aitronssesin <74368135+aitronssesin@users.noreply.github.com>
2023-08-22 20:03:19 +0200

    Delete LOCAL_CREPE_FIX.bat
    Unnecesary file

--- Fixes/LOCAL_CREPE_FIX.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-runtime\python.exe Fixes\local_fixes.py
-
-runtime\python.exe -m pip install --upgrade tensorboard markdown
-
-runtime\python.exe -m pip install -r requirements.txt --upgrade
-
-echo Press Enter to close...
-pause >nul
