29411d2622ccce847bf620e3b2682629d0a933be
aitronssesin <74368135+aitronssesin@users.noreply.github.com>
2023-08-31 01:59:52 +0200

    Add files via upload

--- update_Applio.bat
+++ update_Applio.bat
@@ -1,6 +1,5 @@
 @echo off
-Title Actualizador de Applio
-chcp 65001 > nul
+Title Applio Updater
 setlocal
 
 :::
@@ -9,61 +8,55 @@ setlocal
 :::    /  \   _ __  _ __ | |_  ___
 :::   / /\ \ | '_ \| '_ \| | |/ _ \
 :::  / ____ \| |_) | |_) | | | (_) |
-::: /_/    \_\ .__/| .__/|_|_|\___/ 
-:::          | |   | |                                                                
-:::          |_|   |_|                                                                
+::: /_/    \_\ .__/| .__/|_|_|\___/
+:::          | |   | |
+:::          |_|   |_|
 :::
-::: Versión 1.0.0 - Desarrollado por Aitron
-::: 
+::: Version 1.0.0 - Developed by Aitron
+:::
 
 set "repoUrl=https://github.com/IAHispano/Applio-RVC-Fork/archive/refs/heads/main.zip"
 set "principal=%cd%"
 set "fixesFolder=Fixes"
 set "localFixesPy=local_fixes.py"
-set "subdir=temp_udpate"
+set "subdir=temp_update"
 set "colabmdx=colab_for_mdx.py"
 echo.
 cls
 
-echo Descargando el archivo ZIP...
+for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
+echo.
+
+echo Downloading the ZIP file...
 powershell -command "& { Invoke-WebRequest -Uri '%repoUrl%' -OutFile '%principal%\repo.zip' }"
 echo.
 
-echo Extrayendo archivo ZIP...
+echo Extracting ZIP file...
 powershell -command "& { Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory('%principal%\repo.zip', '%principal%') }"
 echo.
 
-echo Copiando estructura de carpetas y archivos desde el subdirectorio al directorio principal...
+echo Copying folder and file structure from subdirectory to main directory...
 robocopy "%principal%\Applio-RVC-Fork-main" "%principal%" /E
 echo.
 
-echo Eliminando el contenido del subdirectorio (archivos y carpetas)...
+echo Deleting contents of the subdirectory (files and folders)...
 rmdir "%principal%\Applio-RVC-Fork-main" /S /Q
 echo.
 
-echo Limpiando...
+echo Cleaning up...
 del "%principal%\repo.zip"
 echo.
 cls
 
-echo Verificando si el archivo local_fixes.py existe en la carpeta Fixes...
+echo Verifying if the local_fixes.py file exists in the Fixes folder...
 if exist "%fixesFolder%\%localFixesPy%" (
-    echo Ejecutando el archivo...
+    echo Running the file...
     python "%fixesFolder%\%localFixesPy%"
 ) else (
-    echo El archivo "%localFixesBat%" no se encontró en la carpeta "Fixes".
+    echo The file "%localFixesBat%" was not found in the "Fixes" folder.
 )
 
-echo Ejecutando el archivo "%colabmdx%"
-echo Verificando si el archivo "%colabmdx%" existe...
-if exist "%principal%\%colabmdx%" (
-    echo Ejecutando el archivo...
-    python "%principal%\%colabmdx%"
-) else (
-    echo El archivo "%colabmdx%" no se encontró.
-)
-
-echo Applio ha sido actualizado!
+echo Applio has been updated!
 endlocal
 echo.
 pause
