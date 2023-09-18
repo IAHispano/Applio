e3187e9b0cd7e69044176a28a14018c17aec5d93
Aitor Emper <74368135+aitronssesin@users.noreply.github.com>
2023-09-18 22:27:42 +0200

    Update go-applio.bat

--- go-applio.bat
+++ go-applio.bat
@@ -19,6 +19,8 @@ for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
 
 echo [1] Start Applio (Mostly recommended)
 echo [2] Start Applio (DML)
+echo [3] Start Applio (Runtime)
+echo [4] Start Applio (Runtime DML)
 echo.
 
 set /p choice=Select an option: 
@@ -26,7 +28,7 @@ set choice=%choice: =%
 
 if "%choice%"=="1" (
     cls
-    echo WARNING: At this point, it's recommended to disable antivirus or firewall, as errors might occur when downloading pretrained models.
+    echo WARNING: At this point, it's recommended to disable antivirus or firewall.
     echo.
     python infer-web.py --pycmd python --port 7897
     pause
@@ -36,6 +38,7 @@ if "%choice%"=="1" (
 
 if "%choice%"=="2" (
     cls
+    echo WARNING: At this point, it's recommended to disable antivirus or firewall.
     echo.
     python infer-web.py --pycmd python --port 7897 --dml
     pause
@@ -43,8 +46,28 @@ if "%choice%"=="2" (
     goto menu
 )
 
+if "%choice%"=="3" (
+    cls
+    echo WARNING: At this point, it's recommended to disable antivirus or firewall, as errors might occur when downloading pretrained models.
+    echo.
+    python.exe infer-web.py --pycmd runtime/python.exe --port 7897
+    pause
+    cls
+    goto menu
+)
+
+if "%choice%"=="4" (
+    cls
+    echo WARNING: At this point, it's recommended to disable antivirus or firewall.
+    echo.
+    python.exe infer-web.py --pycmd runtime/python.exe --port 7897 --dml
+    pause
+    cls
+    goto menu
+)
+
 cls
-echo Invalid option. Please enter a number from 1 to 5.
+echo Invalid option. Please enter a number from 1 to 4.
 echo.
 echo Press 'Enter' to access the main menu...
 pause>nul
