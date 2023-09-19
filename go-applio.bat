31fefb95ca0dc575ce56882b9db50c7df4b8070f
Bastian Marin <bastyloko11@gmail.com>
2023-09-19 12:26:55 -0300

    Update go-applio.bat
    runtime options will not be shown if you do not have runtime/python.exe installed
    The extra message is not necessary if you use this verification
    (DML will not be understood so I preferred to put Nvidea/AMD supports)

--- go-applio.bat
+++ go-applio.bat
@@ -17,16 +17,23 @@ title Applio - Start
 :menu
 for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
 
-echo [1] Start Applio (Mostly recommended)
-echo [2] Start Applio (DML)
-echo [3] Start Applio (Runtime)
-echo [4] Start Applio (Runtime DML)
+echo [1] Start Applio (Nvidea Support)
+echo [2] Start Applio (AMD Support)
+if exist "runtime\python.exe" (
+    echo ^[3^] Start Applio ^(Runtime Nvidea Support^)
+    echo ^[4^] Start Applio ^(Runtime AMD Support^)
+)
+echo [5] Exit
 echo.
+::echo If you don't know which one to use, try 1 or 3 and the one that doesn't give you errors is the correct one.
+::echo.
 
 set /p choice=Select an option: 
 set choice=%choice: =%
 
-if "%choice%"=="1" (
+if "%choice%"=="5" (
+    goto finish
+) else if "%choice%"=="1" (
     cls
     echo WARNING: At this point, it's recommended to disable antivirus or firewall.
     echo.
@@ -34,9 +41,7 @@ if "%choice%"=="1" (
     pause
     cls
     goto menu
-)
-
-if "%choice%"=="2" (
+) else if "%choice%"=="2" (
     cls
     echo WARNING: At this point, it's recommended to disable antivirus or firewall.
     echo.
@@ -44,26 +49,24 @@ if "%choice%"=="2" (
     pause
     cls
     goto menu
-)
-
-if "%choice%"=="3" (
-    cls
-    echo WARNING: At this point, it's recommended to disable antivirus or firewall, as errors might occur when downloading pretrained models.
-    echo.
-    "runtime/python.exe" infer-web.py --pycmd "runtime/python.exe" --port 7897
-    pause
-    cls
-    goto menu
-)
-
-if "%choice%"=="4" (
-    cls
-    echo WARNING: At this point, it's recommended to disable antivirus or firewall.
-    echo.
-    "runtime/python.exe" infer-web.py --pycmd "runtime/python.exe" --port 7897 --dml
-    pause
-    cls
-    goto menu
+) else if exist "runtime/python.exe" (
+    if "%choice%"=="3" (
+        cls
+        echo WARNING: At this point, it's recommended to disable antivirus or firewall, as errors might occur when downloading pretrained models.
+        echo.
+        runtime\python.exe infer-web.py --pycmd runtime/python.exe --port 7897
+        pause
+        cls
+        goto menu
+    ) else if "%choice%"=="4" (
+        cls
+        echo WARNING: At this point, it's recommended to disable antivirus or firewall.
+        echo.
+        runtime\python.exe infer-web.py --pycmd runtime/python.exe --port 7897 --dml
+        pause
+        cls
+        goto menu
+    )
 )
 
 cls
@@ -73,3 +76,4 @@ echo Press 'Enter' to access the main menu...
 pause>nul
 cls
 goto menu
+:finish
