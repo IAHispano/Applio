fca5886727b667203e12aea2dcbed6f184d171df
DeiantV <vidaldiscord1@gmail.com>
2023-09-24 12:55:33 -0500

    Add Weights Support

--- tabs/resources.py
+++ tabs/resources.py
@@ -285,6 +285,38 @@ def download_from_url(url):
                 wget.download(download_link)
             else:
                 return None
+        elif "www.weights.gg" in url:
+            #Pls weights creator dont fix this because yes. c:
+            url_parts = url.split("/")
+            weights_gg_index = url_parts.index("www.weights.gg")
+            if weights_gg_index != -1 and weights_gg_index < len(url_parts) - 1:
+                model_part = "/".join(url_parts[weights_gg_index + 1:])
+                if "models" in model_part:
+                    model_part = model_part.split("models/")[-1]
+                    print(model_part)
+                    if model_part:
+                        download_url = f"https://www.weights.gg/es/models/{model_part}"
+                        response = requests.get(download_url)
+                        if response.status_code == 200:
+                            soup = BeautifulSoup(response.text, "html.parser")
+                            button_link = soup.find("a", class_="bg-black text-white px-3 py-2 rounded-lg flex items-center gap-1")
+                            if button_link:
+                                download_link = button_link["href"]
+                                result = download_from_url(download_link)
+                                if result == "downloaded":
+                                    return "downloaded"
+                                else:
+                                    return None
+                            else:
+                                return None
+                        else:
+                            return None
+                    else:
+                        return None
+                else:
+                    return None
+            else:
+                return None
         else:
             os.chdir(zips_path)
             wget.download(url)
