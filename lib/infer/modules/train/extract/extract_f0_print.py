6c58f6d5fbd7c767344b0f1265c7ac6fe7c58b00
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-09-26 20:33:50 +0200

    log mesages

--- lib/infer/modules/train/extract/extract_f0_print.py
+++ lib/infer/modules/train/extract/extract_f0_print.py
@@ -237,7 +237,7 @@ class FeatureInput(object):
             printt("no-f0-todo")
             return
         with tqdm.tqdm(total=len(paths), leave=True, position=thread_n) as pbar:
-            description = f"thread:{thread_n}, f0ing, Hop-Length:{crepe_hop_length}"
+            description = f"Thread {thread_n} | Hop-Length: {crepe_hop_length}"
             pbar.set_description(description)
                 
             for idx, (inp_path, opt_path1, opt_path2) in enumerate(paths):
