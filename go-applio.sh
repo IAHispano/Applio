bd704570f1ad3a94a4382669407615fbd44a0330
Aitor Emper <74368135+aitronssesin@users.noreply.github.com>
2023-09-27 19:47:19 +0200

    Update go-applio.sh

--- go-applio.sh
+++ go-applio.sh
@@ -1,56 +1,84 @@
-#!/bin/bash
-echo -e "\033]0;Applio - Start\007"
-export PATH=$HOME/.local/bin/:$PATH
-menu() {
-  while true; do
-    clear
-echo " :::"
-echo " :::                       _ _ "
-echo " :::     /\               | (_) "
-echo " :::    /  \   _ __  _ __ | |_  ___ "
-echo " :::   / /\ \ | '_ \| '_ \| | |/ _ \ "
-echo " :::  / ____ \| |_) | |_) | | | (_) | "
-echo " ::: /_/    \_\ .__/| .__/|_|_|\___/ "
-echo " :::          | |   | | "
-echo " :::          |_|   |_| "
-echo " ::: "
-echo " ::: "
-    echo ""
-    echo "[1] Start Applio (Nvidia Support)"
-    echo "[2] Start Applio (AMD Support)"
-    echo ""
-    echo "[3] Exit"
-    echo ""
+@echo off
+setlocal
+title Applio - Start
 
-    read -p "Select an option: " choice
-    case $choice in
-    1)
-      clear
-      python3.9 infer-web.py --pycmd python --port 7897 --theme dark
-      read -p "Press Enter to continue..."
-      ;;
-    2)
-      clear
-      python3.9 infer-web.py --pycmd python3.9 --port 7897 --dml --theme dark
-      read -p "Press Enter to continue..."
-      ;;
-    3)
-      finish
-      ;;
-    *)
-      clear
-      echo "Invalid option. Please enter a number from 1 to 3."
-      echo ""
-      read -n 1 -s -r -p "Press 'Enter' to access the main menu..."
-      ;;
-    esac
-  done
-}
 
-finish() {
-  clear
-  echo "Exiting Applio..."
-  exit 0
-}
+:::
+:::                       _ _
+:::     /\               | (_)
+:::    /  \   _ __  _ __ | |_  ___
+:::   / /\ \ | '_ \| '_ \| | |/ _ \
+:::  / ____ \| |_) | |_) | | | (_) |
+::: /_/    \_\ .__/| .__/|_|_|\___/
+:::          | |   | |
+:::          |_|   |_|
+:::
+:::
 
-menu
+for /f "usebackq delims=" %%i in ("version.txt") do (
+    set "localVersion=%%i"
+)
+for /f %%i in ('powershell -command "(Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/IAHispano/Applio-RVC-Fork/main/assets/configs/version.txt').Content"') do set "onlineVersion=%%i"
+
+:menu
+for /f "delims=: tokens=*" %%A in ('findstr /b ":::" "%~f0"') do @echo(%%A
+powershell -command "if ('%localVersion%' -lt '%onlineVersion%') { exit 1 } else { exit 0 }"
+if %errorlevel% equ 1 (
+    echo Warning, you are using an outdated version %localVersion%! The version %onlineVersion% is now available.
+    echo.
+    goto continue
+) else (
+    goto continue
+)
+
+:continue
+echo Recommended for regular users:
+echo [1] Start Applio with Runtime (Nvidia Support)
+echo [2] Start Applio with Runtime (AMD Support)
+echo.
+echo Only recommended for experienced users:
+echo [3] Start Applio (Nvidia Support)
+echo [4] Start Applio (AMD Support)
+echo.
+echo [5] Exit
+echo.
+
+set /p choice=Select an option: 
+set choice=%choice: =%
+
+if "%choice%"=="5" (
+    goto finish
+) else if "%choice%"=="4" (
+    cls
+    python infer-web.py --pycmd python --port 7897 --dml --theme dark
+    pause
+    cls
+    goto menu
+) else if "%choice%"=="3" (
+    cls
+    python infer-web.py --pycmd python --port 7897 --theme dark
+    pause
+    cls
+    goto menu
+) else if "%choice%"=="2" (
+    cls
+    runtime\python.exe infer-web.py --pycmd runtime/python.exe --port 7897 --dml --theme dark
+    pause
+    cls
+    goto menu
+) else if "%choice%"=="1" (
+    cls
+    runtime\python.exe infer-web.py --pycmd runtime/python.exe --port 7897 --theme dark
+    pause
+    cls
+    goto menu
+)
+
+cls
+echo Invalid option. Please enter a number from 1 to 5.
+echo.
+echo Press 'Enter' to access the main menu...
+pause>nul
+cls
+goto menu
+:finish
