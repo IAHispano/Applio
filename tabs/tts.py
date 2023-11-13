b5b85416a6643829661c5e137630e7d16efdfc28
DeiantV <raddiscord1@gmail.com>
2023-11-12 22:19:31 -0500

    Delete Bark TTS and Add Google Traductor TTS ( Female )
    I plan to add tortoise or Tacotron to the tts tab, maybe it will be soon.

--- tabs/tts.py
+++ tabs/tts.py
@@ -2,8 +2,6 @@ import sys
 
 sys.path.append("..")
 import os
-os.environ["SUNO_OFFLOAD_CPU"] = "True"
-os.environ["SUNO_USE_SMALL_MODELS"] = "True"
 now_dir = os.getcwd()
 
 from dotenv import load_dotenv
@@ -14,25 +12,23 @@ load_dotenv()
 config = Config()
 vc = VC(config)
 
-import shutil
-import numpy as np
-import torch
-
+import hashlib
+import urllib.parse
 import soundfile as sf
 from gtts import gTTS
 import edge_tts
-import asyncio
 import scipy.io.wavfile as wavfile
 import nltk
-
 nltk.download("punkt", quiet=True)
 from nltk.tokenize import sent_tokenize
-from bark import SAMPLE_RATE
 
+import logging
 import json
 import ssl
 from typing import Any, Dict, List, Optional
 import asyncio
+logging.getLogger('asyncio').setLevel(logging.FATAL)
+
 import aiohttp
 import certifi
 
@@ -41,149 +37,58 @@ VOICE_LIST = (
     + "readaloud/voices/list?trustedclienttoken="
     + "6A5AA1D4EAFF4E9FB37E23D68491D6F4"
 )
-def get_bark_voice():
-    mensaje = """
-v2/en_speaker_0	English	Male
-v2/en_speaker_1	English	Male
-v2/en_speaker_2	English	Male
-v2/en_speaker_3	English	Male
-v2/en_speaker_4	English	Male
-v2/en_speaker_5	English	Male
-v2/en_speaker_6	English	Male
-v2/en_speaker_7	English	Male
-v2/en_speaker_8	English	Male
-v2/en_speaker_9	English	Female
-v2/zh_speaker_0	Chinese (Simplified)	Male
-v2/zh_speaker_1	Chinese (Simplified)	Male
-v2/zh_speaker_2	Chinese (Simplified)	Male
-v2/zh_speaker_3	Chinese (Simplified)	Male
-v2/zh_speaker_4	Chinese (Simplified)	Female
-v2/zh_speaker_5	Chinese (Simplified)	Male
-v2/zh_speaker_6	Chinese (Simplified)	Female
-v2/zh_speaker_7	Chinese (Simplified)	Female
-v2/zh_speaker_8	Chinese (Simplified)	Male
-v2/zh_speaker_9	Chinese (Simplified)	Female
-v2/fr_speaker_0	French	Male
-v2/fr_speaker_1	French	Female
-v2/fr_speaker_2	French	Female
-v2/fr_speaker_3	French	Male
-v2/fr_speaker_4	French	Male
-v2/fr_speaker_5	French	Female
-v2/fr_speaker_6	French	Male
-v2/fr_speaker_7	French	Male
-v2/fr_speaker_8	French	Male
-v2/fr_speaker_9	French	Male
-v2/de_speaker_0	German	Male
-v2/de_speaker_1	German	Male
-v2/de_speaker_2	German	Male
-v2/de_speaker_3	German	Female
-v2/de_speaker_4	German	Male
-v2/de_speaker_5	German	Male
-v2/de_speaker_6	German	Male
-v2/de_speaker_7	German	Male
-v2/de_speaker_8	German	Female
-v2/de_speaker_9	German	Male
-v2/hi_speaker_0	Hindi	Female
-v2/hi_speaker_1	Hindi	Female
-v2/hi_speaker_2	Hindi	Male
-v2/hi_speaker_3	Hindi	Female
-v2/hi_speaker_4	Hindi	Female
-v2/hi_speaker_5	Hindi	Male
-v2/hi_speaker_6	Hindi	Male
-v2/hi_speaker_7	Hindi	Male
-v2/hi_speaker_8	Hindi	Male
-v2/hi_speaker_9	Hindi	Female
-v2/it_speaker_0	Italian	Male
-v2/it_speaker_1	Italian	Male
-v2/it_speaker_2	Italian	Female
-v2/it_speaker_3	Italian	Male
-v2/it_speaker_4	Italian	Male
-v2/it_speaker_5	Italian	Male
-v2/it_speaker_6	Italian	Male
-v2/it_speaker_7	Italian	Female
-v2/it_speaker_8	Italian	Male
-v2/it_speaker_9	Italian	Female
-v2/ja_speaker_0	Japanese	Female
-v2/ja_speaker_1	Japanese	Female
-v2/ja_speaker_2	Japanese	Male
-v2/ja_speaker_3	Japanese	Female
-v2/ja_speaker_4	Japanese	Female
-v2/ja_speaker_5	Japanese	Female
-v2/ja_speaker_6	Japanese	Male
-v2/ja_speaker_7	Japanese	Female
-v2/ja_speaker_8	Japanese	Female
-v2/ja_speaker_9	Japanese	Female
-v2/ko_speaker_0	Korean	Female
-v2/ko_speaker_1	Korean	Male
-v2/ko_speaker_2	Korean	Male
-v2/ko_speaker_3	Korean	Male
-v2/ko_speaker_4	Korean	Male
-v2/ko_speaker_5	Korean	Male
-v2/ko_speaker_6	Korean	Male
-v2/ko_speaker_7	Korean	Male
-v2/ko_speaker_8	Korean	Male
-v2/ko_speaker_9	Korean	Male
-v2/pl_speaker_0	Polish	Male
-v2/pl_speaker_1	Polish	Male
-v2/pl_speaker_2	Polish	Male
-v2/pl_speaker_3	Polish	Male
-v2/pl_speaker_4	Polish	Female
-v2/pl_speaker_5	Polish	Male
-v2/pl_speaker_6	Polish	Female
-v2/pl_speaker_7	Polish	Male
-v2/pl_speaker_8	Polish	Male
-v2/pl_speaker_9	Polish	Female
-v2/pt_speaker_0	Portuguese	Male
-v2/pt_speaker_1	Portuguese	Male
-v2/pt_speaker_2	Portuguese	Male
-v2/pt_speaker_3	Portuguese	Male
-v2/pt_speaker_4	Portuguese	Male
-v2/pt_speaker_5	Portuguese	Male
-v2/pt_speaker_6	Portuguese	Male
-v2/pt_speaker_7	Portuguese	Male
-v2/pt_speaker_8	Portuguese	Male
-v2/pt_speaker_9	Portuguese	Male
-v2/ru_speaker_0	Russian	Male
-v2/ru_speaker_1	Russian	Male
-v2/ru_speaker_2	Russian	Male
-v2/ru_speaker_3	Russian	Male
-v2/ru_speaker_4	Russian	Male
-v2/ru_speaker_5	Russian	Female
-v2/ru_speaker_6	Russian	Female
-v2/ru_speaker_7	Russian	Male
-v2/ru_speaker_8	Russian	Male
-v2/ru_speaker_9	Russian	Female
-v2/es_speaker_0	Spanish	Male
-v2/es_speaker_1	Spanish	Male
-v2/es_speaker_2	Spanish	Male
-v2/es_speaker_3	Spanish	Male
-v2/es_speaker_4	Spanish	Male
-v2/es_speaker_5	Spanish	Male
-v2/es_speaker_6	Spanish	Male
-v2/es_speaker_7	Spanish	Male
-v2/es_speaker_8	Spanish	Female
-v2/es_speaker_9	Spanish	Female
-v2/tr_speaker_0	Turkish	Male
-v2/tr_speaker_1	Turkish	Male
-v2/tr_speaker_2	Turkish	Male
-v2/tr_speaker_3	Turkish	Male
-v2/tr_speaker_4	Turkish	Female
-v2/tr_speaker_5	Turkish	Female
-v2/tr_speaker_6	Turkish	Male
-v2/tr_speaker_7	Turkish	Male
-v2/tr_speaker_8	Turkish	Male
-v2/tr_speaker_9	Turkish	Male
-    """
-    # Dividir el mensaje en líneas
-    lineas = mensaje.split("\n")
-    datos_deseados = []
-    for linea in lineas:
-        partes = linea.split("\t")
-        if len(partes) == 3:
-            clave, _, genero = partes
-            datos_deseados.append(f"{clave}-{genero}")
+class Speech:
+    LANGUAGE_ABBREVIATIONS = {'afrikaans': 'af', 'albanian': 'sq', 'arabic': 'ar', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'chinese (simplified)': 'zh-CN', 'chinese (traditional)': 'zh-TW', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'hebrew': 'iw', 'hindi': 'hi', 'hungarian': 'hu', 'icelandic': 'is', 'indonesian': 'id', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'khmer': 'km', 'korean': 'ko', 'latin': 'la', 'latvian': 'lv', 'malay': 'ms', 'malayalam': 'ml', 'marathi': 'mr', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'polish': 'pl', 'portuguese': 'pt', 'romanian': 'ro', 'russian': 'ru', 'serbian': 'sr', 'sinhala': 'si', 'slovak': 'sk', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'vietnamese': 'vi'}
 
-    return datos_deseados
+    def get_language_abbreviation(self, language):
+        return self.LANGUAGE_ABBREVIATIONS.get(language, language)
+
+    def __init__(self, output, language, proxy, handler):
+        self.folder = output
+        self.language = self.get_language_abbreviation(language.lower())
+        self.proxy = proxy
+        self.handler = handler
+
+    async def create_speech_file(self, text, file_name):
+        try:
+
+            err = await self.download_if_not_exists(file_name, text)
+            if err:
+                return "", err
+
+            return file_name, None
+        except Exception as e:
+            return "", e
+
+    async def speak(self, text):
+        try:
+            file_name, err = await self.create_speech_file(text, self.folder)
+            if err:
+                raise err
+            return file_name
+        except Exception as e:
+            return e
+
+
+    async def download_if_not_exists(self, file_name, text):
+        try:
+            if not os.path.exists(file_name):
+                dl_url = f"http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q={urllib.parse.quote(text)}&tl={self.language}"
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(dl_url) as response:
+                        if response.status != 200:
+                            raise Exception(f"HTTP error: {response.status}")
+                        content = await response.read()
+                        with open(file_name, "wb") as output:
+                            output.write(content)
+                print("Downloaded file:", file_name)
+            return None
+        except Exception as e:
+            print("Exception during download:", e)
+            return e
+
+def get_google_voice():
+    return list(Speech.LANGUAGE_ABBREVIATIONS.keys())
 
 # ||-----------------------------------------------------------------------------------||
 # ||                         Obtained from dependency edge_tts                         ||
@@ -241,120 +146,50 @@ async def loop_main():
     voices_json = json.dumps(voices)
     return voices_json
 
-def get_edge_voice():
-    loop = asyncio.get_event_loop()
-    voices_json = loop.run_until_complete(loop_main())
-    voices = json.loads(voices_json)
-    tts_voice = []
-    for voice in voices:
-        short_name = voice['ShortName']
-        gender = voice['Gender']
-        formatted_entry = f"{short_name}-{gender}"
-        tts_voice.append(formatted_entry)
-       # print(f"{short_name}-{gender}")
-    return tts_voice
 
-set_bark_voice = get_bark_voice()
+def get_edge_voice():
+    try:
+        loop = asyncio.get_event_loop()
+        voices_json = loop.run_until_complete(loop_main())
+        #loop.close()
+        voices = json.loads(voices_json)
+        tts_voice = []
+        for voice in voices:
+            short_name = voice['ShortName']
+            gender = voice['Gender']
+            formatted_entry = f"{short_name}-{gender}"
+            tts_voice.append(formatted_entry)
+            # print(f"{short_name}-{gender}")
+        return tts_voice
+    except Exception as e:
+        if isinstance(e, asyncio.exceptions.CancelledError):
+            pass
+        else:
+            return []
+
+set_google_voice = get_google_voice()
 set_edge_voice = get_edge_voice()
 
 def update_tts_methods_voice(select_value):
     # ["Edge-tts", "RVG-tts", "Bark-tts"]
     if select_value == "Edge-tts":
         return {"choices": set_edge_voice, "value": "", "__type__": "update"}
-    elif select_value == "Bark-tts":
-        return {"choices": set_bark_voice, "value": "", "__type__": "update"}
+    elif select_value == "Google-tts":
+        return {"choices": set_google_voice, "value": "", "__type__": "update"}
 
 
-def custom_voice(
-    _values,  # filter indices
-    audio_files,  # all audio files
-    model_voice_path="",
-    transpose=0,
-    f0method="pm",
-    index_rate_=float(0.66),
-    crepe_hop_length_=float(64),
-    f0_autotune=False,
-    file_index="",
-    file_index2="",
-):
-    vc.get_vc(model_voice_path)
-
-    for _value_item in _values:
-        filename = (
-            "assets/audios/audio_outputs" + audio_files[_value_item]
-            if _value_item != "converted_tts"
-            else audio_files[0]
-        )
-        # filename = "audio2/"+audio_files[_value_item]
-        try:
-            print(audio_files[_value_item], model_voice_path)
-        except:
-            pass
-        info_, (sample_, audio_output_) = vc.vc_single_dont_save(
-            sid=0,
-            input_audio_path1=filename,  # f"audio2/{filename}",
-            f0_up_key=transpose,  # transpose for m to f and reverse 0 12
-            f0_file=None,
-            f0_method=f0method,
-            file_index=file_index,  # dir pwd?
-            file_index2=file_index2,
-            # file_big_npy1,
-            index_rate=index_rate_,
-            filter_radius=int(3),
-            resample_sr=int(0),
-            rms_mix_rate=float(0.25),
-            protect=float(0.33),
-            crepe_hop_length=crepe_hop_length_,
-            f0_autotune=f0_autotune,
-            f0_min=50,
-            note_min=50,
-            f0_max=1100,
-            note_max=1100,
-        )
-
-        sf.write(
-            file=filename,  # f"audio2/{filename}",
-            samplerate=sample_,
-            data=audio_output_,
-        )
-
-
-def cast_to_device(tensor, device):
+async def process_google_tts(language, tts_text, output_folder):
     try:
-        return tensor.to(device)
+        script = tts_text.replace("\n", " ").strip()
+        speech = Speech(output=output_folder, language=language, proxy=None, handler=None)
+        google_out_filename = await speech.speak(script)
+        print(google_out_filename)
+        return google_out_filename
+
     except Exception as e:
-        print(e)
-        return tensor
-
-
-def __bark__(text, voice_preset):
-    os.makedirs(os.path.join(now_dir, "tts"), exist_ok=True)
-    from transformers import AutoProcessor, BarkModel
-
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    dtype = torch.float32 if "cpu" in device else torch.float16
-    bark_processor = AutoProcessor.from_pretrained(
-        "suno/bark",
-        cache_dir=os.path.join(now_dir, "tts", "suno/bark"),
-        torch_dtype=dtype,
-    )
-    bark_model = BarkModel.from_pretrained(
-        "suno/bark",
-        cache_dir=os.path.join(now_dir, "tts", "suno/bark"),
-        torch_dtype=dtype,
-    ).to(device)
-    # bark_model.enable_cpu_offload()
-    inputs = bark_processor(text=[text], return_tensors="pt", voice_preset=voice_preset)
-    tensor_dict = {
-        k: cast_to_device(v, device) if hasattr(v, "to") else v
-        for k, v in inputs.items()
-    }
-    speech_values = bark_model.generate(**tensor_dict, do_sample=True)
-    sampling_rate = bark_model.generation_config.sample_rate
-    speech = speech_values.cpu().numpy().squeeze()
-    return speech, sampling_rate
-
-
+        print(f"{e}")
+        return None
+    
 def use_tts(
     tts_text,
     tts_voice,
@@ -372,21 +207,25 @@ def use_tts(
 
     output_folder = "assets/audios/audio-outputs"
     os.makedirs(output_folder, exist_ok=True)
-    output_count = 1  # Contador para nombres de archivo únicos
+    output_count = 1
 
     while True:
         converted_tts_filename = os.path.join(output_folder, f"tts_out_{output_count}.wav")
-        bark_out_filename = os.path.join(output_folder, f"bark_out_{output_count}.wav")
         
-        if not os.path.exists(converted_tts_filename) and not os.path.exists(bark_out_filename):
+        if not os.path.exists(converted_tts_filename):
+            break
+        output_count += 1
+    output_count = 1
+    while True:
+        google_out_filename = os.path.join(output_folder, f"google_out_{output_count}.wav")
+        
+        if not os.path.exists(google_out_filename):
             break
         output_count += 1
     
-    
-    if "SET_LIMIT" == os.getenv("DEMO"):
-        if len(tts_text) > 60:
-            tts_text = tts_text[:60]
-            print("DEMO; limit to 60 characters")
+    if len(tts_text) > 3900 and tts_method == "Google-tts":
+        tts_text = tts_text[:3900]
+        print("Google Traductor; limit to 3900 characters")
 
     language = tts_voice[:2]
     if tts_method == "Edge-tts":
@@ -432,11 +271,7 @@ def use_tts(
                 f0_max=1100,
                 note_max=1100,
             )
-
-            # Genera un nombre de archivo único para el archivo procesado por vc.vc_single_dont_save
             vc_output_filename = os.path.join(output_folder, f"converted_tts_{output_count}.wav")
-            
-            # Guarda el archivo de audio procesado por vc.vc_single_dont_save
             wavfile.write(
                 vc_output_filename,
                 rate=sample_,
@@ -448,26 +283,17 @@ def use_tts(
             print(f"{e}")
             return None, None
 
-    elif tts_method == "Bark-tts":
+    elif tts_method == "Google-tts":
         try:
-            script = tts_text.replace("\n", " ").strip()
-            sentences = sent_tokenize(script)
-            print(sentences)
-            silence = np.zeros(int(0.25 * SAMPLE_RATE))
-            pieces = []
-            for sentence in sentences:
-                audio_array, _ = __bark__(sentence, tts_voice.split("-")[0])
-                pieces += [audio_array, silence.copy()]
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            google_out_filename = loop.run_until_complete(process_google_tts(tts_voice, tts_text, google_out_filename))
+            loop.close()
 
-            sf.write(
-                file=bark_out_filename, samplerate=SAMPLE_RATE, data=np.concatenate(pieces)
-            )
             vc.get_vc(model_path)
             info_, (sample_, audio_output_) = vc.vc_single_dont_save(
                 sid=0,
-                input_audio_path1=os.path.join(
-                    now_dir, "assets", "audios", "audio-outputs", "bark_out.wav"
-                ),  # f"audio2/{filename}",
+                input_audio_path1=google_out_filename,  # f"audio2/{filename}",
                 f0_up_key=transpose,  # transpose for m to f and reverse 0 12
                 f0_file=None,
                 f0_method=f0_method,
@@ -487,16 +313,14 @@ def use_tts(
                 note_max=1100,
             )
             
-            vc_output_filename = os.path.join(output_folder, f"converted_bark_{output_count}.wav")
-            
-            # Guarda el archivo de audio procesado por vc.vc_single_dont_save
+            vc_output_filename = os.path.join(output_folder, f"converted_google_{output_count}.wav")
             wavfile.write(
                 vc_output_filename,
                 rate=sample_,
                 data=audio_output_,
             )
 
-            return vc_output_filename, bark_out_filename
+            return vc_output_filename, google_out_filename
 
         except Exception as e:
             print(f"{e}")
