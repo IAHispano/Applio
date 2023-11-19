53c51d26727c3d0cb0e3340efd34c4932e6d635d
Aitor Emper <74368135+aitronssesin@users.noreply.github.com>
2023-11-19 22:18:23 +0100

    Upgrade pip to fix some errors

--- install_Applio.bat
+++ install_Applio.bat
@@ -146,6 +146,7 @@ call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
 conda install -c anaconda git -y
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" deactivate
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
+pip install --upgrade pip
 pip install -r %principal%\assets\requirements\requirements.txt
 pip install future==0.18.2
 pip uninstall torch torchvision torchaudio -y
@@ -181,6 +182,7 @@ call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
 conda install -c anaconda git -y
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" deactivate
 call "%CONDA_ROOT_PREFIX%\condabin\conda.bat" activate "%INSTALL_ENV_DIR%"
+pip install --upgrade pip
 pip uninstall onnxruntime onnxruntime-directml
 pip install -r %principal%\assets\requirements\requirements.txt
 pip install -r %principal%\assets\requirements\requirements-dml.txt
