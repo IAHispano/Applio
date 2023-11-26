7e2b331f93022957d4544fb79f8988d8bad68289
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-11-26 01:13:10 +0100

    optimize load_audio

--- lib/infer/infer_libs/audio.py
+++ lib/infer/infer_libs/audio.py
@@ -1,107 +1,91 @@
-import librosa
-import numpy as np
-import av
-from io import BytesIO
-import ffmpeg
 import os
+import subprocess
+from io import BytesIO
 import traceback
 import sys
-
-from lib.infer.infer_libs.csvutil import CSVutil
-#import csv
+import av
+import librosa
+import numpy as np
+import ffmpeg
 
 platform_stft_mapping = {
-    'linux': 'stftpitchshift',
-    'darwin': 'stftpitchshift',
-    'win32': 'stftpitchshift.exe',
+    "linux": "stftpitchshift",
+    "darwin": "stftpitchshift",
+    "win32": "stftpitchshift.exe",
 }
 
 stft = platform_stft_mapping.get(sys.platform)
 
-def wav2(i, o, format):
-    inp = av.open(i, 'rb')
-    if format == "m4a": format = "mp4"
-    out = av.open(o, 'wb', format=format)
-    if format == "ogg": format = "libvorbis"
-    if format == "mp4": format = "aac"
+
+def wav2(input_file, output_file, format):
+    inp = av.open(input_file, "rb")
+    if format == "m4a":
+        format = "mp4"
+    out = av.open(output_file, "wb", format=format)
+    if format == "ogg":
+        format = "libvorbis"
+    if format == "mp4":
+        format = "aac"
 
     ostream = out.add_stream(format)
 
     for frame in inp.decode(audio=0):
-        for p in ostream.encode(frame): out.mux(p)
+        for p in ostream.encode(frame):
+            out.mux(p)
 
-    for p in ostream.encode(None): out.mux(p)
+    for p in ostream.encode(None):
+        out.mux(p)
 
     out.close()
     inp.close()
 
-def audio2(i, o, format, sr):
-    inp = av.open(i, 'rb')
-    out = av.open(o, 'wb', format=format)
-    if format == "ogg": format = "libvorbis"
-    if format == "f32le": format = "pcm_f32le"
+
+def audio2(input_file, output_file, format, sr):
+    inp = av.open(input_file, "rb")
+    out = av.open(output_file, "wb", format=format)
+    if format == "ogg":
+        format = "libvorbis"
+    if format == "f32le":
+        format = "pcm_f32le"
 
     ostream = out.add_stream(format, channels=1)
     ostream.sample_rate = sr
 
     for frame in inp.decode(audio=0):
-        for p in ostream.encode(frame): out.mux(p)
+        for p in ostream.encode(frame):
+            out.mux(p)
 
     out.close()
     inp.close()
 
-def load_audion(file, sr):
-    try:
-        file = (
-            file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
-        )  # 防止小白拷路径头尾带了空格和"和回车
-        with open(file, "rb") as f:
-            with BytesIO() as out:
-                audio2(f, out, "f32le", sr)
-                return np.frombuffer(out.getvalue(), np.float32).flatten()
-
-    except AttributeError:
-        audio = file[1] / 32768.0
-        if len(audio.shape) == 2:
-            audio = np.mean(audio, -1)
-        return librosa.resample(audio, orig_sr=file[0], target_sr=16000)
-
-    except Exception as e:
-        raise RuntimeError(f"Failed to load audio: {e}")
-
-
-
 
 def load_audio(file, sr, DoFormant=False, Quefrency=1.0, Timbre=1.0):
-    file = (
-            file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
-        )
-    if os.path.exists(file) == False:
+    file = file.strip(' \n"')
+    if not os.path.exists(file):
         raise RuntimeError(
-            "You input a wrong audio path that does not exists, please fix it!"
+            "Wrong audio path, that does not exist."
         )
+
     converted = False
     try:
         if not file.endswith(".wav"):
             converted = True
-            # Conversión de formato usando ffmpeg
-            converting = (
-                ffmpeg.input(file, threads=0)
-                .output(f"{file}.wav")
-                .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+            subprocess.run(
+                ["ffmpeg", "-nostdin", "-i", file, f"{file}.wav"],
+                capture_output=True,
+                text=True,
             )
             file = f"{file}.wav"
-            print(f" · File converted to Wav format: {file}\n")
+            print(f"File formatted to wav format: {file}\n")
 
-        if DoFormant == True:
-            # Procesamiento de formantes usando stftpitchshift
+        if DoFormant:
             command = (
                 f'{stft} -i "{file}" -q "{Quefrency}" '
-                f'-t "{Timbre}" -o "{file}FORMANTED.wav"'
+                f'-t "{Timbre}" -o "{file}_formatted.wav"'
             )
-            os.system(command)
-            file = f"{file}FORMANTED.wav"
-            print(f" · Formanted {file}!\n")
+            subprocess.run(command, shell=True)
+            file = f"{file}_FORMANTED.wav"
+            print(f"Formatted {file}\n")
 
         with open(file, "rb") as f:
             with BytesIO() as out:
@@ -109,31 +93,33 @@ def load_audio(file, sr, DoFormant=False, Quefrency=1.0, Timbre=1.0):
                 audio_data = np.frombuffer(out.getvalue(), np.float32).flatten()
 
         if converted:
-            try: os.remove(file)
-            except Exception as e: pass; print(f"Couldn't remove converted type of file due to {e}")
+            try:
+                os.remove(file)
+            except Exception as error:
+                print(f"Couldn't remove converted type of file due to {error}")
+                error = None
             converted = False
 
         return audio_data
+
     except AttributeError:
         audio = file[1] / 32768.0
         if len(audio.shape) == 2:
             audio = np.mean(audio, -1)
         return librosa.resample(audio, orig_sr=file[0], target_sr=16000)
-    except:
+    except Exception:
         raise RuntimeError(traceback.format_exc())
 
 
 def check_audio_duration(file):
     try:
-        file = file.strip(" ").strip('"').strip("\n").strip('"').strip(" ")
-
+        file = file.strip(' \n"')
         probe = ffmpeg.probe(file)
-
-        duration = float(probe['streams'][0]['duration'])
+        duration = float(probe["streams"][0]["duration"])
 
         if duration < 0.76:
             print(
-                f"Audio file, {file.split('/')[-1]}, under ~0.76s detected - file is too short. Target at least 1-2s for best results."
+                f"Audio file, {os.path.basename(file)}, under ~0.76s detected - file is too short. Target at least 1-2s for best results."
             )
             return False
 
