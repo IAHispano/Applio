68828d2adaf92ad7d5e854b4fe57c5094187b105
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-11-29 01:47:01 +0100

    go tensorboard linux (untested I dont have linux)

---
+++ go-tensorboard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "\033]0;Applio-RVC-Fork - Tensorboard\007"
+source .venv/bin/activate
+clear
+
+echo "Starting the Tensorboard with Conda..."
+clear
+echo "Starting Tensorboard..."
+pip install tensorboard
+python3.9 lib/fixes/tensor-launch.py
+
+read -p "Press Enter to exit..."
+exit 0
