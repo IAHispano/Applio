a989920eef3efdcbe5e949c698734124bb456213
Anthony <144498068+Anthonyxd22@users.noreply.github.com>
2023-12-03 10:14:59 -0500

    Update go-tensorboard.sh Fix
    to avoid unnecessary reinstallation etc...

--- go-tensorboard.sh
+++ go-tensorboard.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
+CONDA_ENV=".venv"
 echo -e "\033]0;Applio-RVC-Fork - Tensorboard\007"
-source .venv/bin/activate
-clear
+     
+ if [ -d "$CONDA_ENV" ]; then
+    source "$CONDA_ENV/bin/activate"
+    echo "Activated Conda environment."
+    else
+    echo "Conda environment not found. Please ensure it's set up properly."
+    exit 1
+    fi
 
-echo "Starting the Tensorboard with Conda..."
-clear
-echo "Starting Tensorboard..."
-pip install tensorboard
-python lib/fixes/tensor-launch.py
 
-read -p "Press Enter to exit..."
-exit 0
+    clear
+   
+      
+    if ! command -v tensorboard &> /dev/null; then
+    echo "Installing Tensorboard..."
+    pip install tensorboard || { echo "Tensorboard installation failed."; exit 1; }
+    fi
+
+
+ echo "Starting Tensorboard..."
+ python lib/fixes/tensor-launch.py || { echo "Failed to start Tensorboard."; exit 1; }
+
+
+ read -p "Press Enter to exit..."
+
+ exit 0
