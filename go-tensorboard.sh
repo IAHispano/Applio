8efbfc69d7d45c0b5d5b210cb20a12ff589eb1d7
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-08 13:33:30 +0100

    Fix go-tensorboard.sh

--- go-tensorboard.sh
+++ go-tensorboard.sh
@@ -7,7 +7,7 @@ echo "Starting the Tensorboard with Conda..."
 clear
 echo "Starting Tensorboard..."
 pip install tensorboard
-python lib/fixes/tensor-launch.py
+python lib/tools/tensorLaunch.py
 
 read -p "Press Enter to exit..."
 exit 0
