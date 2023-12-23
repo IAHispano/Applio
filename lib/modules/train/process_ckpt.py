3b28d90248d792cf27275362fc27c47f2a4beeca
Blaise <133521603+blaise-tk@users.noreply.github.com>
2023-12-24 00:41:49 +0100

    changed ckpt info

--- lib/modules/train/process_ckpt.py
+++ lib/modules/train/process_ckpt.py
@@ -37,7 +37,7 @@ def savee(ckpt, sr, if_f0, name, epoch, version, hps):
             hps.model.gin_channels,
             hps.data.sampling_rate,
         ]
-        opt["info"] = "%sepoch" % epoch
+        opt["info"] = epoch
         opt["sr"] = sr
         opt["f0"] = if_f0
         opt["version"] = version
