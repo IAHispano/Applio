67b8f31c21a84c107f8df377ffa000b9f7d3dff9
Blaise <133521603+blaise-tk@users.noreply.github.com>
2024-01-21 19:56:25 +0100

    Update i18n.py

--- assets/i18n/i18n.py
+++ assets/i18n/i18n.py
@@ -7,16 +7,12 @@ class I18nAuto:
 
     def __init__(self, language=None):
         language = language or getdefaultlocale()[0]
-
         
         lang_prefix = language[:2] if language is not None else "en"
-
-       
         available_languages = self._get_available_languages()
-
         matching_languages = [lang for lang in available_languages if lang.startswith(lang_prefix)]
+        
         self.language = matching_languages[0] if matching_languages else "en_US"
-
         self.language_map = self._load_language_list()
 
     def _load_language_list(self):
@@ -38,7 +34,3 @@ class I18nAuto:
 
     def __call__(self, key):
         return self.language_map.get(key, key)
-
-    def print(self):
-        print(f"Using Language: {self.language}")
-
