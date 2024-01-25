567868be9f9ef0e5c1e850983c51d8713f7c7260
ddf420 <42910674+ddf420@users.noreply.github.com>
2024-01-25 22:24:26 +0800

    Changed model extraction output path to logs/model_name/model_name.pth

--- rvc/train/process_ckpt.py
+++ rvc/train/process_ckpt.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import pathlib
 from collections import OrderedDict
 
 logs_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "logs")
@@ -203,7 +204,8 @@ def extract_small_model(path, name, sr, if_f0, info, version):
             sr,
             int(if_f0),
         )
-        torch.save(opt, f"logs/weights/{name}.pth")
+        pathlib.Path(f"logs/{name}/").mkdir(parents=True, exist_ok=True) 
+        torch.save(opt, f"logs/{name}/{name}.pth")
         return "Success."
     except Exception as error:
         print(error)
