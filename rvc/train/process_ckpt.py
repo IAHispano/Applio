81d6e5c2675dfb3e02726db926aa9a6610b75e77
ddf420 <42910674+ddf420@users.noreply.github.com>
2024-01-25 22:37:00 +0800

    Removed redundant folder creation check

--- rvc/train/process_ckpt.py
+++ rvc/train/process_ckpt.py
@@ -204,7 +204,6 @@ def extract_small_model(path, name, sr, if_f0, info, version):
             sr,
             int(if_f0),
         )
-        pathlib.Path(f"logs/{name}/").mkdir(parents=True, exist_ok=True) 
         torch.save(opt, f"logs/{name}/{name}.pth")
         return "Success."
     except Exception as error:
