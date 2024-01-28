546324b39d0e9f3f8065c4d4260e3748e1a4afed
aitronssesin <aitronssesin@gmail.com>
2024-01-28 21:36:35 +0100

    Fix

--- rvc/infer/infer.py
+++ rvc/infer/infer.py
@@ -222,9 +222,10 @@ audio_output_path = sys.argv[7]
 
 model_path = sys.argv[8]
 index_path = sys.argv[9]
-split_audio = sys.argv[10]
 
-if split_audio == "False":
+try:
+    split_audio = sys.argv[10]
+except IndexError:
     split_audio = None
 
 sid = f0up_key
