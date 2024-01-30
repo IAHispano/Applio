a56e823aef02741a47cb3f122106e4d568e15ee2
aitronssesin <aitronssesin@gmail.com>
2024-01-31 00:09:14 +0100

    Fix google colab

--- app.py
+++ app.py
@@ -5,20 +5,20 @@ import os
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
+class InstallationError(Exception):
+    def __init__(self, message="InstallationError"):
+        self.message = message
+        super().__init__(self.message)
+
 try:
     system_drive = os.getenv('SystemDrive')
     current_drive = os.path.splitdrive(now_dir)[0]
+    if current_drive.upper() != system_drive.upper():
+        raise InstallationError(f"Error: Current working directory is not on the default system drive ({system_drive}). Please move Applio in the correct drive.")
 except:
     pass
 else:
-    class InstallationError(Exception):
-        def __init__(self, message="InstallationError"):
-            self.message = message
-            super().__init__(self.message)
-
-    if current_drive.upper() != system_drive.upper():
-        raise InstallationError(f"Error: Current working directory is not on the default system drive ({system_drive}). Please move Applio in the correct drive.")
-    elif "OneDrive" in now_dir:
+    if "OneDrive" in now_dir:
         raise InstallationError("Error: Current working directory is on OneDrive. Please move Applio in another folder.")
     elif " " in now_dir:
         raise InstallationError("Error: Current working directory contains spaces. Please move Applio in another folder.")
