dba37708e28f1dcd879b244733d7282dd93ac7ca
aitronssesin <aitronssesin@gmail.com>
2024-01-30 23:31:44 +0100

    Add fix for colab

--- app.py
+++ app.py
@@ -5,24 +5,27 @@ import os
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
-system_drive = os.getenv('SystemDrive')
-current_drive = os.path.splitdrive(now_dir)[0]
-
-class InstallationError(Exception):
-    def __init__(self, message="InstallationError"):
-        self.message = message
-        super().__init__(self.message)
-
-if current_drive.upper() != system_drive.upper():
-    raise InstallationError(f"Error: Current working directory is not on the default system drive ({system_drive}). Please move Applio in the correct drive.")
-elif "OneDrive" in now_dir:
-    raise InstallationError("Error: Current working directory is on OneDrive. Please move Applio in another folder.")
-elif " " in now_dir:
-    raise InstallationError("Error: Current working directory contains spaces. Please move Applio in another folder.")
 try:
-    now_dir.encode('ascii')
-except UnicodeEncodeError:
-    raise InstallationError("Error: Current working directory contains non-ASCII characters. Please move Applio in another folder.")
+    system_drive = os.getenv('SystemDrive')
+    current_drive = os.path.splitdrive(now_dir)[0]
+except Exception as e:
+    pass
+else:
+    class InstallationError(Exception):
+        def __init__(self, message="InstallationError"):
+            self.message = message
+            super().__init__(self.message)
+
+    if current_drive.upper() != system_drive.upper():
+        raise InstallationError(f"Error: Current working directory is not on the default system drive ({system_drive}). Please move Applio in the correct drive.")
+    elif "OneDrive" in now_dir:
+        raise InstallationError("Error: Current working directory is on OneDrive. Please move Applio in another folder.")
+    elif " " in now_dir:
+        raise InstallationError("Error: Current working directory contains spaces. Please move Applio in another folder.")
+    try:
+        now_dir.encode('ascii')
+    except UnicodeEncodeError:
+        raise InstallationError("Error: Current working directory contains non-ASCII characters. Please move Applio in another folder.")
 
 from tabs.inference.inference import inference_tab
 from tabs.train.train import train_tab
