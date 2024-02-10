18ba34a37d85ddf579f0dc8cbc79086cb7ecffb7
rappc87 <ozgur.avcil.006@gmail.com>
2024-02-10 19:12:55 +0300

    Fix
    Fixed the problem that Index file is not unloaded when unloaded. and index is now automatically selected when the model is selected.

--- tabs/tts/tts.py
+++ tabs/tts/tts.py
@@ -197,7 +197,15 @@ def process_input(file_path):
     gr.Info(f"The text from the txt file has been loaded!")
     return file_contents, None
 
-
+def match_index(model_file_value):
+    if model_file_value:
+        model_folder = os.path.dirname(model_file_value)
+        index_files = get_indexes()
+        for index_file in index_files:
+            if os.path.dirname(index_file) == model_folder:
+                return index_file
+    return ""
+    
 def tts_tab():
     default_weight = random.choice(names) if names else ""
     with gr.Row():
@@ -222,13 +230,13 @@ def tts_tab():
             unload_button = gr.Button(i18n("Unload Voice"))
 
             unload_button.click(
-                fn=lambda: ({"value": "", "__type__": "update"}),
+                fn=lambda: ({"value": "", "__type__": "update"}, {"value": "", "__type__": "update"}),
                 inputs=[],
-                outputs=[model_file],
+                outputs=[model_file, index_file],
             )
 
             model_file.select(
-                fn=match_index,
+                fn=lambda model_file_value: match_index(model_file_value),
                 inputs=[model_file],
                 outputs=[index_file],
             )
