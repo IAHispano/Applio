38cd249fa3973cc2ff627546d72771574778832d
rappc87 <ozgur.avcil.006@gmail.com>
2024-02-10 11:54:23 +0300

    Add files via upload

--- tabs/settings/presence.py
+++ tabs/settings/presence.py
@@ -5,6 +5,7 @@ import pathlib
 import tempfile
 import gradio as gr
 import threading
+import json
 from assets.i18n.i18n import I18nAuto
 from assets.discord_presence import RPCManager
 
@@ -13,6 +14,24 @@ sys.path.append("..")
 
 i18n = I18nAuto()
 
+CONFIG_FILE = "config.json" 
+
+
+def load_config():
+    default_config = {"enable_presence": True}  
+    if os.path.exists(CONFIG_FILE):
+        with open(CONFIG_FILE, "r") as file:
+            return json.load(file)
+    return default_config
+
+
+def save_config(config):
+    with open(CONFIG_FILE, "w") as file:
+        json.dump(config, file)
+
+
+config = load_config()
+
 
 def presence_tab():
     with gr.Row():
@@ -20,7 +39,7 @@ def presence_tab():
             presence = gr.Checkbox(
                 label=i18n("Enable Applio integration with Discord presence"),
                 interactive=True,
-                value=True,
+                value=config["enable_presence"],
             )
             presence.change(
                 fn=toggle,
@@ -30,13 +49,12 @@ def presence_tab():
 
 
 def toggle(checkbox):
-
-    if bool(checkbox):
-        # print("Start Presence")
+    config["enable_presence"] = bool(checkbox) 
+    save_config(config)
+    if config["enable_presence"]:
         try:
             RPCManager.start_presence()
         except KeyboardInterrupt:
             RPCManager.stop_presence()
     else:
-        # print("Stop presence")
         RPCManager.stop_presence()
