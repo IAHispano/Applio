4ad134aed9b40ad627b73592a590218d4fc3c63e
rappc87 <ozgur.avcil.006@gmail.com>
2024-02-11 15:03:45 +0300

    restored to default version
    restart applio button moved to a different tab.

--- tabs/settings/version.py
+++ tabs/settings/version.py
@@ -1,18 +1,9 @@
 import gradio as gr
-import os
-import sys
-
-def restart_applio():
-    if os.name == "nt": 
-        os.system("cls")
-    python = sys.executable
-    os.execl(python, python, *sys.argv)
 
 from assets.version_checker import compare_version
 from assets.i18n.i18n import I18nAuto
 
 i18n = I18nAuto()
-
 
 def version_tab():
     with gr.Row():
@@ -26,10 +17,4 @@ def version_tab():
                 fn=compare_version,
                 inputs=[],
                 outputs=[version_check],
-            )
-            restart_button = gr.Button(i18n("Restart Applio"))
-            restart_button.click(
-                fn=restart_applio,
-                inputs=[],
-                outputs=[],
             )
