db0e387b22f420ee9e6a065d3efe1b51299f938b
Blaise <blaise@applio.org>
2024-06-17 09:00:16 +0200

    v3 extracted folder

--- rvc/train/process/extract_index.py
+++ rvc/train/process/extract_index.py
@@ -10,7 +10,7 @@ version = sys.argv[2]
 
 try:
     feature_dir = os.path.join(exp_dir, f"{version}_extracted")
- 
+
     npys = []
     listdir_res = sorted(os.listdir(feature_dir))
 
@@ -50,9 +50,7 @@ try:
     index_ivf_trained.nprobe = 1
     index_trained.train(big_npy)
 
-    index_filename_trained = (
-        f"trained_{version}.index"
-    )
+    index_filename_trained = f"trained_{version}.index"
     index_filepath_trained = os.path.join(exp_dir, index_filename_trained)
 
     faiss.write_index(index_trained, index_filepath_trained)
@@ -65,9 +63,7 @@ try:
     index_ivf_added.nprobe = 1
     index_added.train(big_npy)
 
-    index_filename_added = (
-        f"added_{version}.index"
-    )
+    index_filename_added = f"added_{version}.index"
     index_filepath_added = os.path.join(exp_dir, index_filename_added)
 
     batch_size_add = 8192
