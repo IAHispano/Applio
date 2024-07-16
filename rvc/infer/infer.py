46b532e7f22aa65fb581c6d72626d44c1ac82943
Blaise <blaise@applio.org>
2024-07-16 23:40:54 +0200

    fix split

--- rvc/infer/infer.py
+++ rvc/infer/infer.py
@@ -195,7 +195,7 @@ class VoiceConverter:
             if self.tgt_sr != resample_sr >= 16000:
                 self.tgt_sr = resample_sr
 
-            if split_audio:
+            if split_audio == "True":
                 result, new_dir_path = process_audio(audio_input_path)
                 if result == "Error":
                     return "Error with Split Audio", None
