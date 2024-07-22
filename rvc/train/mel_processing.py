bb9b34428ef42be9ca08d583bf9dadef95705dc5
Alexey Shmelev <aznamir@gmail.com>
2024-07-22 14:07:54 -0400

    typo fixed

--- rvc/train/mel_processing.py
+++ rvc/train/mel_processing.py
@@ -79,7 +79,7 @@ def spectrogram_torch(y, n_fft, hop_size, win_size, center=False):
     # Zluda fall-back to CPU for FFTs since HIP SDK has no cuFFT alternative
     source_device = y.device
     if y.device.type == "cuda" and torch.cuda.get_device_name(y.device.index).endswith("[ZLUDA]"):
-        y = y.to("cpu"),
+        y = y.to("cpu")
         hann_window[wnsize_dtype_device] = hann_window[wnsize_dtype_device].to("cpu")
     
     spec = torch.stft(
