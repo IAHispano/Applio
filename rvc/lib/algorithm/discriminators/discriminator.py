38036291c345d5cf70921689ac82816b6d0f579f
Vidalnt <raddiscord1@gmail.com>
2024-08-08 18:20:49 -0500

    Fix Combined

--- rvc/lib/algorithm/discriminators/discriminator.py
+++ rvc/lib/algorithm/discriminators/discriminator.py
@@ -1,10 +1,21 @@
-import torch.nn as nn
-class CombinedDiscriminator(nn.Module):
+import torch
+class CombinedDiscriminator(torch.nn.Module):
     def __init__(self, discriminators):
         super().__init__()
-        self.discriminators = nn.ModuleList(discriminators)
+        self.discriminators = torch.nn.ModuleList(discriminators)
 
     def forward(self, y, y_hat):
-        os = [d(y, y_hat) for d in self.discriminators]
-        return [sum(o, []) for o in zip(*os)]
+        y_d_rs = []
+        y_d_gs = []
+        fmap_rs = []
+        fmap_gs = []
+
+        for disc in self.discriminators:
+            y_d_r, y_d_g, fmap_r, fmap_g = disc(y, y_hat)
+            y_d_rs.extend(y_d_r)
+            fmap_rs.extend(fmap_r)
+            y_d_gs.extend(y_d_g)
+            fmap_gs.extend(fmap_g)
+
+        return y_d_rs, y_d_gs, fmap_rs, fmap_gs
 
