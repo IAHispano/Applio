85afd062633be0cece0234a342ee7b070d6c7635
hpx <50779358+hpx502766238@users.noreply.github.com>
2024-08-10 20:35:31 +0800

    Update utils.py
    solved re-download problem for online embedders

--- rvc/lib/utils.py
+++ rvc/lib/utils.py
@@ -58,11 +58,11 @@ def load_embedding(embedder_model, custom_embedder=None):
             model_path = embedding_list["contentvec"]
     else:
         model_path = embedding_list[embedder_model]
-        if embedder_model in online_embedders:
-            model_path = embedding_list[embedder_model]
-            url = online_embedders[embedder_model]
-            print(f"\nDownloading {url} to {model_path}...")
-            wget.download(url, out=model_path)
+        if embedder_model in online_embedders:  
+            if not os.path.exists(model_path):
+                url = online_embedders[embedder_model]
+                print(f"\nDownloading {url} to {model_path}...")
+                wget.download(url, out=model_path)
         else:
             model_path = embedding_list["contentvec"]
 
