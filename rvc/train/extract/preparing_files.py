47cb5857243832cc9b16dd118b971d25697835f1
Blaise <blaise@applio.org>
2024-08-15 16:48:51 +0200

    improve gen list logic

--- rvc/train/extract/preparing_files.py
+++ rvc/train/extract/preparing_files.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 from random import shuffle
-
 from rvc.configs.config import Config
 
 config = Config()
@@ -18,43 +17,53 @@ def generate_config(rvc_version: str, sample_rate: int, model_path: str):
 def generate_filelist(
     pitch_guidance: bool, model_path: str, rvc_version: str, sample_rate: int
 ):
-    gt_wavs_dir = f"{model_path}/sliced_audios"
-    feature_dir = (
-        f"{model_path}/v1_extracted"
-        if rvc_version == "v1"
-        else f"{model_path}/v2_extracted"
-    )
-    if pitch_guidance == True:
-        f0_dir = f"{model_path}/f0"
-        f0nsf_dir = f"{model_path}/f0_voiced"
-        names = (
-            set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)])
-            & set([name.split(".")[0] for name in os.listdir(feature_dir)])
-            & set([name.split(".")[0] for name in os.listdir(f0_dir)])
-            & set([name.split(".")[0] for name in os.listdir(f0nsf_dir)])
-        )
-    elif pitch_guidance == False:
-        names = set([name.split(".")[0] for name in os.listdir(gt_wavs_dir)]) & set(
-            [name.split(".")[0] for name in os.listdir(feature_dir)]
-        )
+    gt_wavs_dir = os.path.join(model_path, "sliced_audios")
+    feature_dir = os.path.join(model_path, f"{rvc_version}_extracted")
+
+    f0_dir, f0nsf_dir = None, None
+    if pitch_guidance:
+        f0_dir = os.path.join(model_path, "f0")
+        f0nsf_dir = os.path.join(model_path, "f0_voiced")
+
+    gt_wavs_files = set(name.split(".")[0] for name in os.listdir(gt_wavs_dir))
+    feature_files = set(name.split(".")[0] for name in os.listdir(feature_dir))
+
+    if pitch_guidance:
+        f0_files = set(name.split(".")[0] for name in os.listdir(f0_dir))
+        f0nsf_files = set(name.split(".")[0] for name in os.listdir(f0nsf_dir))
+        names = gt_wavs_files & feature_files & f0_files & f0nsf_files
+    else:
+        names = gt_wavs_files & feature_files
+
     options = []
+    mute_base_path = os.path.join(current_directory, "logs", "mute")
+
     for name in names:
-        if pitch_guidance == 1:
+        if pitch_guidance:
             options.append(
                 f"{gt_wavs_dir}/{name}.wav|{feature_dir}/{name}.npy|{f0_dir}/{name}.wav.npy|{f0nsf_dir}/{name}.wav.npy|0"
             )
         else:
             options.append(f"{gt_wavs_dir}/{name}.wav|{feature_dir}/{name}.npy|0")
-    if pitch_guidance == 1:
-        for _ in range(2):
+
+    mute_audio_path = os.path.join(
+        mute_base_path, "sliced_audios", f"mute{sample_rate}.wav"
+    )
+    mute_feature_path = os.path.join(
+        mute_base_path, f"{rvc_version}_extracted", "mute.npy"
+    )
+
+    for _ in range(2):
+        if pitch_guidance:
+            mute_f0_path = os.path.join(mute_base_path, "f0", "mute.wav.npy")
+            mute_f0nsf_path = os.path.join(mute_base_path, "f0_voiced", "mute.wav.npy")
             options.append(
-                f"{current_directory}/logs/mute/sliced_audios/mute{sample_rate}.wav|{current_directory}/logs/mute/{rvc_version}_extracted/mute.npy|{current_directory}/logs/mute/f0/mute.wav.npy|{current_directory}/logs/mute/f0_voiced/mute.wav.npy|0"
+                f"{mute_audio_path}|{mute_feature_path}|{mute_f0_path}|{mute_f0nsf_path}|0"
             )
-    else:
-        for _ in range(2):
-            options.append(
-                f"{current_directory}/logs/mute/sliced_audios/mute{sample_rate}.wav|{current_directory}/logs/mute/{rvc_version}_extracted/mute.npy|0"
-            )
+        else:
+            options.append(f"{mute_audio_path}|{mute_feature_path}|0")
+
     shuffle(options)
-    with open(f"{model_path}/filelist.txt", "w") as f:
+
+    with open(os.path.join(model_path, "filelist.txt"), "w") as f:
         f.write("\n".join(options))
