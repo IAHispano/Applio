56aa4e8d8baa7d68c1e0e84fd89fd6276b8a6ea1
Guilherme <86894155+ShiromiyaG@users.noreply.github.com>
2024-08-24 16:34:32 -0300

    Call hubertmodel directly

--- rvc/lib/utils.py
+++ rvc/lib/utils.py
@@ -8,7 +8,6 @@ import wget
 import subprocess
 from pydub import AudioSegment
 import tempfile
-from torch import nn
 
 import logging
 from transformers import HubertModel
@@ -26,12 +25,6 @@ sys.path.append(now_dir)
 
 base_path = os.path.join(now_dir, "rvc", "models", "formant", "stftpitchshift")
 stft = base_path + ".exe" if sys.platform == "win32" else base_path
-
-
-class HubertModelWithFinalProj(HubertModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.final_proj = nn.Linear(config.hidden_size, config.classifier_proj_size)
 
 def load_audio(file, sample_rate):
     try:
@@ -147,6 +140,6 @@ def load_embedding(embedder_model, custom_embedder=None):
             print(f"Downloading {url} to {model_path}...")
             wget.download(url, out=json_file)
 
-    models = HubertModelWithFinalProj.from_pretrained(model_path)
+    models = HubertModel.from_pretrained(model_path)
 
     return models
