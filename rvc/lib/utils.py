b10c3f429c2342aecae90651ffb990c52777cf24
Guilherme <86894155+ShiromiyaG@users.noreply.github.com>
2024-08-23 21:21:32 -0300

    Fix

--- rvc/lib/utils.py
+++ rvc/lib/utils.py
@@ -128,18 +128,19 @@ def load_embedding(embedder_model, custom_embedder=None):
             model_path = embedding_list["contentvec"]
     else:
         model_path = embedding_list[embedder_model]
-        if embedder_model in online_embedders:
-            if not os.path.exists(model_path):
-                url = online_embedders[embedder_model]
-                print(f"Downloading {url} to {model_path}...")
-                wget.download(url, out=model_path)
-                url = config_files[embedder_model]
-                print(f"Downloading {url} to {model_path}...")
-                wget.download(url, out=model_path)
+        bin_file = os.path.join(model_path, 'pytorch_model.bin')
+        json_file = os.path.join(model_path, 'config.json')
+        if not os.path.exists(bin_file) or not os.path.exists(json_file):
+            url = online_embedders[embedder_model]
+            print(f"Downloading {url} to {model_path}...")
+            wget.download(url, out=bin_file)
+            url = config_files[embedder_model]
+            print(f"Downloading {url} to {model_path}...")
+            wget.download(url, out=json_file)
 
 
     if custom_embedder:
-        models = HubertModelWithFinalProj.from_pretrained(embedder_root, custom_embedder)
+        models = HubertModelWithFinalProj.from_pretrained(os.path.join(embedder_root, custom_embedder))
     else:
         models = HubertModelWithFinalProj.from_pretrained(model_path)
 
