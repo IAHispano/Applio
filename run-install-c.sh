c0a01140f7bf0bb28f35c22efb6d35b542b55f22
Vidalnt <raddiscord1@gmail.com>
2024-09-08 19:01:49 -0500

    Update run-install-c.sh

--- run-install-c.sh
+++ run-install-c.sh
@@ -9,7 +9,7 @@ echo
 principal=$(pwd)
 CONDA_ROOT_PREFIX="$HOME/Miniconda3"
 INSTALL_ENV_DIR="$principal/env"
-MINICONDA_DOWNLOAD_URL="https://repo.anaconda.com/miniconda/Miniconda3-py39_23.9.0-0-Linux-x86_64.sh"
+MINICONDA_DOWNLOAD_URL="https://repo.anaconda.com/miniconda/Miniconda3-py39_24.7.1-0-Linux-x86_64.sh"
 CONDA_EXECUTABLE="$CONDA_ROOT_PREFIX/bin/conda"
 
 echo "Cleaning up unnecessary files..."
