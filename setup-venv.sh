2d94bf3cfb0f3850867780fd2856465338bb165d
Vidalnt <raddiscord1@gmail.com>
2024-09-11 18:32:42 -0500

    Update setup-venv.sh

--- setup-venv.sh
+++ setup-venv.sh
@@ -3,10 +3,7 @@
 printf "\033]0;Applio\007"
 
 echo "Checking for a suitable Python interpreter..."
-if command -v python3.10 > /dev/null 2>&1; then
-  py=$(which python3.10)
-  echo "Using python3.10"
-elif command -v python3 > /dev/null 2>&1; then
+if command -v python3 > /dev/null 2>&1; then
   py=$(which python3)
   echo "Using python3 (Note: Python 3.10 is recommended)"
 elif command -v python > /dev/null 2>&1; then
@@ -20,8 +17,7 @@ fi
 echo "Recreating virtual environment..."
 $py -m venv .venv
 
-current_dir=$(pwd)
-find ".venv" -type f -exec sed -i -e 's/\r$//' -e "s|/home/runner/work/Applio/Applio/|$current_dir/|g" -e "s|/.venv/bin/python|/.venv/bin/$(basename $py)|g" {} +
+find ".venv" -type f -exec sed -i -e 's/\r$//' -e "s|/home/runner/work/Applio/Applio|$(pwd)|g" -e "s|/.venv/bin/python|/.venv/bin/$(basename $py)|g" {} +
 echo "Virtual environment paths fixed."
 
 echo "Done."
