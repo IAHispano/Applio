e05bc0fc52cdc041dc6294de89d181910df0f181
Blaise <blaise@applio.org>
2024-12-22 22:10:48 +0100

    improve pretrained selector

--- rvc/lib/tools/pretrained_selector.py
+++ rvc/lib/tools/pretrained_selector.py
@@ -1,8 +1,8 @@
 import os
 
 def pretrained_selector(version, vocoder, pitch_guidance, sample_rate):
-    path = f"rvc/models/pretraineds/pretrained_{version}/"
-    f0 = "f0" if pitch_guidance == True else ""
+    base_path = os.path.join("rvc", "models", "pretraineds", f"pretrained_{version}")
+    f0 = "f0" if pitch_guidance else ""
 
     if vocoder == "HiFi-GAN":
         vocoder_path = ""
@@ -10,9 +10,11 @@ def pretrained_selector(version, vocoder, pitch_guidance, sample_rate):
         vocoder_path = "HiFiGAN_"
     elif vocoder == "RefineGAN":
         vocoder_path = "RefineGAN_"
+    else:
+        vocoder_path = ""
 
-    path_g = f"{path}{vocoder_path}{f0}G{str(sample_rate)[:2]}k.pth"
-    path_d = f"{path}{vocoder_path}{f0}D{str(sample_rate)[:2]}k.pth"
+    path_g = os.path.join(base_path, f"{vocoder_path}{f0}G{str(sample_rate)[:2]}k.pth")
+    path_d = os.path.join(base_path, f"{vocoder_path}{f0}D{str(sample_rate)[:2]}k.pth")
 
     if os.path.exists(path_g) and os.path.exists(path_d):
         return path_g, path_d
