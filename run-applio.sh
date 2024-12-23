140ff47ef29c7b1d5cd73477033dba2c6cfefb9e
Anthony <144498068+Marxanthonydev@users.noreply.github.com>
2024-12-23 17:52:28 -0500

    Update run-applio.sh Fix macOS

--- run-applio.sh
+++ run-applio.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+\#!/bin/sh
 printf "\033]0;Applio\007"
 . .venv/bin/activate
 
  export PYTORCH_ENABLE_MPS_FALLBACK=1
  export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
+ export OMP_NUM_THREADS=8 
  
 clear
 python app.py --open
