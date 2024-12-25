8b5769914c9dfc4ccaa8f7bdd68d6c723b970002
Ali <vynz420@gmail.com>
2024-12-25 12:40:19 +0500

    api_update

--- api.py
+++ api.py
@@ -20,33 +20,34 @@ default_sid = get_speakers_id(default_model_file)[0] if get_speakers_id(default_
 output_tts_path = os.path.join(os.getcwd(), "assets", "audios", "tts_output.wav")
 output_rvc_path = os.path.join(os.getcwd(), "assets", "audios", "tts_rvc_output.wav")
 
+# Input model
 class TTSRequest(BaseModel):
-    text: str
+    tts_text: str  # Only text is required
 
 
 @app.post("/tts")
 async def tts_endpoint(request: TTSRequest):
     try:
-        # Call the `run_tts_script` with default and user-provided values
+        # Run the TTS script with default parameters
         output_info, audio_file_path = run_tts_script(
-            tts_file='',  # Not using file upload for this endpoint
-            tts_text=request.text,
+            tts_file=None,
+            tts_text=request.tts_text,
             tts_voice=default_voice_name,
             tts_rate=0,  # Default TTS speed
             pitch=0,  # Default pitch
             filter_radius=3,
             index_rate=0.75,
-            rms_mix_rate=1,
+            volume_envelope=1,
             protect=0.5,
             hop_length=128,
             f0_method="rmvpe",
             output_tts_path=output_tts_path,
             output_rvc_path=output_rvc_path,
-            model_file=default_model_file,
-            index_file=default_index_file,
+            pth_path=default_model_file,
+            index_path=default_index_file,
             split_audio=False,
-            autotune=False,
-            autotune_strength=1,
+            f0_autotune=False,
+            f0_autotune_strength=1.0,
             clean_audio=True,
             clean_strength=0.5,
             export_format="WAV",
