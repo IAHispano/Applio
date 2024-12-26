e83807900fab0185be2326a416690fb913f2c485
Anthony <144498068+MarxAnthony@users.noreply.github.com>
2024-12-26 14:38:57 -0500

    Update run-applio.sh

--- run-applio.sh
+++ run-applio.sh
@@ -4,7 +4,7 @@ printf "\033]0;Applio\007"
 
 export PYTORCH_ENABLE_MPS_FALLBACK=1
 export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 
 clear
 python app.py --open
