41cd9dbf1d6305e5b5f23ac3efd20ba7cc78b9f4
AznamirWoW <101997116+AznamirWoW@users.noreply.github.com>
2025-01-15 20:15:53 -0500

    updated ddsp

--- rvc/lib/algorithm/generators/ddsp.py
+++ rvc/lib/algorithm/generators/ddsp.py
@@ -2,8 +2,9 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.checkpoint import checkpoint
 
-def oscillate_impluse(f0: torch.Tensor, hop_size: int, sample_rate: float):
+def oscillate_impluse(f0: torch.Tensor, sample_rate: float):
     '''
     f0: [N, 1, L]
     hop_size: int
@@ -11,7 +12,7 @@ def oscillate_impluse(f0: torch.Tensor, hop_size: int, sample_rate: float):
 
     Output: [N, 1, L * hop_size]
     '''
-    f0 = F.interpolate(f0, scale_factor=hop_size, mode='linear')
+    
     I = torch.cumsum(f0, dim=2)
     sawtooth = (I / sample_rate) % 1.0
     impluse = sawtooth - sawtooth.roll(-1, dims=(2)) + (f0 / sample_rate)
@@ -70,19 +71,20 @@ class ConvNeXtLayer(nn.Module):
         x = x + res
         return x
 
-
 class DDSPGenerator(nn.Module):
     def __init__(
         self,
         n_mels=192,
         internal_channels=512,
         num_layers=8,
-        n_fft=1920,
+        n_fft=4096, #1920,
         sample_rate=48000,
         gin_channels=256,
+        checkpointing: bool = False,
     ):
         super().__init__()
-        
+        self.checkpointing = checkpointing
+
         self.sample_rate = sample_rate
         self.hop_size = sample_rate // 100
         self.n_fft = n_fft
@@ -95,39 +97,51 @@ class DDSPGenerator(nn.Module):
         self.to_phase = nn.Conv1d(internal_channels, self.n_fft // 2 + 1, 1)
         
         self.cond = nn.Conv1d(gin_channels, internal_channels, 1)
-    
+        
     def forward(self, x, f0, g):
-    
+        # x: (B, 192, 36), f0: (B, 36), g: (B, 256, 1)
+        
+        # (B, 192, 36) -> (B, 512, 36)
         x = self.input_layer(x)
         x += self.cond(g)
         x = self.mid_layers(x)
 
-        per = F.sigmoid(self.conv_post(x))
-
+        # (B, 961, 36)
         mag = self.to_mag(x)
+        # (B, 961, 36)
         phase = self.to_phase(x)
-        kernel = torch.exp(mag) * torch.exp(phase * 1j)
-        
-        impluse = oscillate_impluse(f0.unsqueeze(1), self.hop_size, self.sample_rate)
-        noise = torch.randn_like(impluse)
-        
+        # (B, 961, 36) complex64
+        kernel = torch.exp_(mag) * torch.exp_(phase * 1j)
+        del mag, phase
+        #print(x.shape)
+        # (B, 512, 36) -> (B, 1, 36)
+        per = F.sigmoid(self.conv_post(x))
+        # (B, 1, 36) -> (B, 1, 17280)
         per = F.interpolate(per, scale_factor=self.hop_size, mode='linear')
-        wf = (per * impluse + (1-per) * noise).squeeze(1)
-        
-        wf_stft = torch.stft(
-            wf, 
+        # (B, 36) -> (B, 1, 17280)
+        f0 = F.interpolate(f0.unsqueeze(1), scale_factor=self.hop_size, mode='linear')
+        impulse = oscillate_impluse(f0, self.sample_rate)
+        # voiced/unvoiced helper
+        wave = (per * impulse + (1 - per) * torch.randn_like(impulse)).squeeze(1)
+      
+        # stft
+        # in: (B, C, T)
+        # out: (B, n_fft / 2, 1 + T / hop_size)
+        wave_stft = torch.stft(
+            wave, 
             self.n_fft,
             self.hop_size,
-            window=self.window.to(wf.device),
-            return_complex=True)[:, :, 1:]
-        
-        out_stft = F.pad(wf_stft * kernel, [0, 1])
-
+            window=self.window.to(wave.device),
+            return_complex=True,
+            # center = True -- default
+            # onesided = True -- default
+            )[:, :, 1:]
+       
+        out_stft = F.pad(wave_stft * kernel, [0, 1])
         out = torch.istft(
             out_stft, 
             self.n_fft,
             self.hop_size,
-            window=self.window.to(wf.device)
+            window=self.window.to(wave.device)
         ).unsqueeze(-2)
-
         return out
