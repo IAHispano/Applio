2a9f4fa2a13271a50551704b733f942fe7614a67
kubinka0505 <kubinka0505@yandex.com>
2025-07-13 11:20:54 +0200

    Update f0_extractor.py
    - Extract F0 Curve button will not be clickable if:
      - file is invalid

--- tabs/extra/sections/f0_extractor.py
+++ tabs/extra/sections/f0_extractor.py
@@ -4,11 +4,20 @@ import gradio as gr
 from matplotlib import pyplot as plt
 
 from rvc.lib.predictors.F0Extractor import F0Extractor
+from tabs.inference.inference import sup_audioext
 
 from assets.i18n.i18n import I18nAuto
 
 i18n = I18nAuto()
 
+def validate_upload(path):
+    if all((
+		path,
+		os.path.isfile(path),
+		path.lower().endswith(sup_audioext)
+	)):
+        return gr.update(interactive=True)
+    return gr.update(interactive=False)
 
 def extract_f0_curve(audio_path: str, method: str):
     print("Extracting F0 Curve...")
@@ -50,7 +59,7 @@ def f0_extractor_tab():
         choices=["crepe", "fcpe", "rmvpe"],
         value="rmvpe",
     )
-    button = gr.Button(i18n("Extract F0 Curve"))
+    button = gr.Button(i18n("Extract F0 Curve"), interactive=False)
 
     with gr.Row():
         txt_output = gr.File(label="F0 Curve", type="filepath")
@@ -63,4 +72,10 @@ def f0_extractor_tab():
             f0_method,
         ],
         outputs=[image_output, txt_output],
+    )
+
+    audio.change(
+        fn=validate_upload,
+        inputs=[audio],
+        outputs=[button],
     )
