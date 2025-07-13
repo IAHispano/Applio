36f8fb3113bb72a2e0c512d242e1e549a28d4a6a
kubinka0505 <kubinka0505@yandex.com>
2025-07-13 11:37:37 +0200

    Update processing.py
    - View button is not clickable if:
      - Path to model is invalid

--- tabs/extra/sections/processing.py
+++ tabs/extra/sections/processing.py
@@ -6,10 +6,15 @@ now_dir = os.getcwd()
 sys.path.append(now_dir)
 
 from core import run_model_information_script
+from tabs.inference.inference import sup_audioext
 from assets.i18n.i18n import I18nAuto
 
 i18n = I18nAuto()
 
+def validate_upload(path):
+    if path.lower().endswith(".pth") and os.path.isfile(path):
+        return gr.update(interactive=True)
+    return gr.update(interactive=False)
 
 def processing_tab():
     model_view_model_path = gr.Textbox(
@@ -26,9 +31,21 @@ def processing_tab():
         value="",
         max_lines=11,
     )
-    model_view_button = gr.Button(i18n("View"))
+    model_view_button = gr.Button(i18n("View"), interactive=False)
     model_view_button.click(
         fn=run_model_information_script,
         inputs=[model_view_model_path],
         outputs=[model_view_output_info],
+    )
+
+    model_view_model_path.change(
+        fn=validate_upload,
+        inputs=[model_view_model_path],
+        outputs=[model_view_button],
+    )
+
+    model_view_model_path.change(
+        fn=validate_upload,
+        inputs=[model_view_model_path],
+        outputs=[model_view_button],
     )
