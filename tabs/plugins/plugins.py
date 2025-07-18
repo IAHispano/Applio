e954c780903b366fea02a2e373ae1f4e5ba65549
kubinka0505 <kubinka0505@yandex.com>
2025-07-18 21:40:48 +0200

    fix 2

--- tabs/plugins/plugins.py
+++ tabs/plugins/plugins.py
@@ -11,10 +11,10 @@ now_dir = os.getcwd()
 sys.path.append(now_dir)
 
 plugins_core.check_new_folders()
-
+main_func_name = "applio_plugin"
 
 def plugins_tab():
-    with gr.TabItem(i18n("Plugin Installer")):
+    with gr.Tab(label=i18n("Plugin Installer")):
         dropbox = gr.File(
             label=i18n("Drag your plugin.zip to install it"),
             type="filepath",
@@ -26,12 +26,33 @@ def plugins_tab():
             outputs=[dropbox],
         )
 
-    for plugin in os.listdir(os.path.join(now_dir, "tabs", "plugins", "installed")):
-        if os.path.isfile(plugin):
+    plugins_dir = os.path.join(now_dir, "tabs", "plugins", "installed")
+
+    for plugin in os.listdir(plugins_dir):
+        plugin_path = os.path.join(plugins_dir, plugin)
+        plugin_name = os.path.splitext(os.path.basename(plugin_path))[0]
+
+        if not os.path.isdir(plugin_path) or plugin.startswith("."):
             continue
 
         plugin_main = f"tabs.plugins.installed.{plugin}.plugin"
-        plugin_import = importlib.import_module(plugin_main)
 
-        with gr.TabItem(plugin):
-            plugin_import.applio_plugin()
+        try:
+            plugin_import = importlib.import_module(plugin_main)
+
+            if hasattr(plugin_import, main_func_name):
+                with gr.Tab(label=plugin):
+                    plugin_import.applio_plugin()
+            else:
+                raise gr.Error(
+                    i18n('Plugin "{plugin_name}" missing {func_name}()').format(
+                        plugin_name = plugin_name,
+                        func_name = main_func_name
+                    )
+                )
+        except Exception as e:
+            raise gr.Error(
+                i18n('Error loading plugin "{plugin_name}":').format(
+                    plugin_name = plugin_name
+                ) + " " + e
+            )
