fixed change_rms and added remove gradients

rvc/realtime/pipeline.py
@@ -247,88 +247,90 @@ class Realtime_Pipeline:
         """
         Performs realtime voice conversion on a given audio segment.
         """
-        assert audio.dim() == 1, audio.dim()
-        feats = audio.view(1, -1).to(self.device)
+        with torch.no_grad():
+            assert audio.dim() == 1, audio.dim()
+            feats = audio.view(1, -1).to(self.device)
 
-        formant_length = int(np.ceil(return_length * 1.0))
+            formant_length = int(np.ceil(return_length * 1.0))
 
-        pitch, pitchf = (
-            self.get_f0(
-                audio[silence_front:],
-                pitch,
-                pitchf,
-                f0_up_key,
-                f0_autotune,
-                f0_autotune_strength,
-                proposed_pitch,
-                proposed_pitch_threshold,
-            )
-            if self.use_f0
-            else (None, None)
-        )
-
-        # extract features
-        feats = self.hubert_model(feats)["last_hidden_state"]
-        feats = (
-            self.hubert_model.final_proj(feats[0]).unsqueeze(0)
-            if self.version == "v1"
-            else feats
-        )
-
-        feats = torch.cat((feats, feats[:, -1:, :]), 1)
-        # make a copy for pitch guidance and protection
-        feats0 = feats.detach().clone() if self.use_f0 else None
-
-        if (
-            self.index
-        ):  # set by parent function, only true if index is available, loaded, and index rate > 0
-            feats = self._retrieve_speaker_embeddings(
-                skip_head, feats, self.index, self.big_npy, index_rate
-            )
-        # feature upsampling
-        feats = F.interpolate(feats.permute(0, 2, 1), scale_factor=2).permute(0, 2, 1)[
-            :, :p_len, :
-        ]
-
-        if self.use_f0:
-            feats0 = F.interpolate(feats0.permute(0, 2, 1), scale_factor=2).permute(
-                0, 2, 1
-            )[:, :p_len, :]
-            pitch, pitchf = pitch[:, -p_len:], pitchf[:, -p_len:] * (
-                formant_length / return_length
-            )
-
-            # Pitch protection blending
-            if protect < 0.5:
-                pitchff = pitchf.detach().clone()
-                pitchff[pitchf > 0] = 1
-                pitchff[pitchf < 1] = protect
-                feats = feats * pitchff.unsqueeze(-1) + feats0 * (
-                    1 - pitchff.unsqueeze(-1)
+            pitch, pitchf = (
+                self.get_f0(
+                    audio[silence_front:],
+                    pitch,
+                    pitchf,
+                    f0_up_key,
+                    f0_autotune,
+                    f0_autotune_strength,
+                    proposed_pitch,
+                    proposed_pitch_threshold,
                 )
-                feats = feats.to(feats0.dtype)
-        else:
-            pitch, pitchf = None, None
-
-        p_len = torch.tensor([p_len], device=self.device, dtype=torch.int64)
-        out_audio = self.vc.inference(feats, p_len, self.sid, pitch, pitchf).float()
-        if volume_envelope != 1:
-            out_audio = AudioProcessor.change_rms(
-                audio, self.sample_rate, out_audio, self.tgt_sr, volume_envelope
+                if self.use_f0
+                else (None, None)
             )
 
-        scaled_window = int(np.floor(1.0 * self.model_window))
-
-        if scaled_window != self.model_window:
-            if scaled_window not in self.resamplers:
-                self.resamplers[scaled_window] = tat.Resample(
-                    orig_freq=scaled_window,
-                    new_freq=self.model_window,
-                    dtype=torch.float32,
-                ).to(self.device)
-            out_audio = self.resamplers[scaled_window](
-                out_audio[: return_length * scaled_window]
+            # extract features
+            feats = self.hubert_model(feats)["last_hidden_state"]
+            feats = (
+                self.hubert_model.final_proj(feats[0]).unsqueeze(0)
+                if self.version == "v1"
+                else feats
             )
+
+            feats = torch.cat((feats, feats[:, -1:, :]), 1)
+            # make a copy for pitch guidance and protection
+            feats0 = feats.detach().clone() if self.use_f0 else None
+
+            if (
+                self.index
+            ):  # set by parent function, only true if index is available, loaded, and index rate > 0
+                feats = self._retrieve_speaker_embeddings(
+                    skip_head, feats, self.index, self.big_npy, index_rate
+                )
+            # feature upsampling
+            feats = F.interpolate(feats.permute(0, 2, 1), scale_factor=2).permute(0, 2, 1)[
+                :, :p_len, :
+            ]
+
+            if self.use_f0:
+                feats0 = F.interpolate(feats0.permute(0, 2, 1), scale_factor=2).permute(
+                    0, 2, 1
+                )[:, :p_len, :]
+                pitch, pitchf = pitch[:, -p_len:], pitchf[:, -p_len:] * (
+                    formant_length / return_length
+                )
+
+                # Pitch protection blending
+                if protect < 0.5:
+                    pitchff = pitchf.detach().clone()
+                    pitchff[pitchf > 0] = 1
+                    pitchff[pitchf < 1] = protect
+                    feats = feats * pitchff.unsqueeze(-1) + feats0 * (
+                        1 - pitchff.unsqueeze(-1)
+                    )
+                    feats = feats.to(feats0.dtype)
+            else:
+                pitch, pitchf = None, None
+
+            p_len = torch.tensor([p_len], device=self.device, dtype=torch.int64)
+            out_audio = self.vc.inference(feats, p_len, self.sid, pitch, pitchf).float()
+            if volume_envelope != 1:
+                out_audio = AudioProcessor.change_rms(
+                    audio.cpu().numpy(), self.sample_rate, out_audio.cpu().numpy(), self.tgt_sr, volume_envelope
+                )
+                out_audio = torch.as_tensor(out_audio, device=self.device)
+
+            scaled_window = int(np.floor(1.0 * self.model_window))
+
+            if scaled_window != self.model_window:
+                if scaled_window not in self.resamplers:
+                    self.resamplers[scaled_window] = tat.Resample(
+                        orig_freq=scaled_window,
+                        new_freq=self.model_window,
+                        dtype=torch.float32,
+                    ).to(self.device)
+                out_audio = self.resamplers[scaled_window](
+                    out_audio[: return_length * scaled_window]
+                )
 
         return out_audio
 