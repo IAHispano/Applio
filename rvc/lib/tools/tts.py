d3b9e4d7c9252945f1b9aeaa626aa7a7a33d6d01
Vidalnt <102625768+Vidalnt@users.noreply.github.com>
2026-04-02 17:39:25 -0500

    fix: handle exceptions directly in UI scripts
    - inference
    - tts
    Initially attempted to move error handling to core.py, but overlooked that the existing code already handles exception propagation. Relocating capture to inference.py and tts.py for proper UI feedback.

--- rvc/lib/tools/tts.py
+++ rvc/lib/tools/tts.py
@@ -1,11 +1,9 @@
-import sys
 import asyncio
-import edge_tts
 import os
-
+import sys
+import edge_tts
 
 async def main():
-    # Parse command line arguments
     tts_file = str(sys.argv[1])
     text = str(sys.argv[2])
     voice = str(sys.argv[3])
@@ -13,16 +11,19 @@ async def main():
     output_file = str(sys.argv[5])
 
     rates = f"+{rate}%" if rate >= 0 else f"{rate}%"
+
     if tts_file and os.path.exists(tts_file):
-        text = ""
         try:
             with open(tts_file, "r", encoding="utf-8") as file:
                 text = file.read()
-        except UnicodeDecodeError:
+        except:
             with open(tts_file, "r") as file:
                 text = file.read()
-    await edge_tts.Communicate(text, voice, rate=rates).save(output_file)
-    # print(f"TTS with {voice} completed. Output TTS file: '{output_file}'")
+
+    try:
+        await edge_tts.Communicate(text, voice, rate=rates).save(output_file)
+    except Exception:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
