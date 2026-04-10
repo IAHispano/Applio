6042bf92e02e5c34ed196086e44ab408fdd05093
Vidalnt <102625768+Vidalnt@users.noreply.github.com>
2026-04-10 14:55:05 -0500

    Update run-install.sh

--- run-install.sh
+++ run-install.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 printf "\033]0;Installer\007"
 clear
 
-# Delete Windows bat files (.bat) 
+# Delete Windows bat files (.bat)
 find . -type f -iname "*.bat" -delete
 
 # Function to log messages with timestamps
